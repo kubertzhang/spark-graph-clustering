bfb709a0b79b0d1a1d2bb453232939b32326ce6a
kubertzhang <doerzhang@163.com>
2018-11-12 22:49:19 +0800

    update sources codes 11.12

--- src/main/scala/EdgeWeightUpdate.scala
+++ src/main/scala/EdgeWeightUpdate.scala
@@ -1,9 +1,8 @@
 import org.apache.spark.graphx._
-import scala.reflect.ClassTag
 
 object EdgeWeightUpdate {
-  def updateEdgeWeight[VD: ClassTag, ED: ClassTag](
-    graph: Graph[VD, ED],
+  def updateEdgeWeight(
+    edgeWeightUpdateGraph: Graph[(Long, Long), Double],
     oldEdgeWeights: Array[Double]): Array[Double] ={
 
     val newEdgeWeights = Array(1.0, 1.2, 0.8, 1.0)
